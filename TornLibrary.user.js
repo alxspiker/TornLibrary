0a7958fe069b37da4a5c23a0918bb90d5a212023
alxspiker <alxtheprogrammer@gmail.com>
2025-07-27 22:28:29 -0600

    Update TornLibrary.user.js

--- TornLibrary.user.js
+++ TornLibrary.user.js
@@ -1953,7 +1953,12 @@ TornLibrary.page.common = {
                     scriptsSection.id = 'tl-scripts-section';
                     scriptsSection.className = 'sidebar-block___Ef1l1 desktop___aYLqo';
                     scriptsSection.innerHTML = `<div class="content___wSUdj"><div class="toggle-block___oKpdF"><div class="header___RpWar desktop___ei8Er"><h2 class="title___XfwKa">Scripts</h2></div><div class="toggle-content___BJ9Q9" id="tl-scripts-links-container"></div></div></div>`;
-                    sidebarElement.insertBefore(scriptsSection, sidebarElement.children[sidebarElement.children.length - 1]);
+                    const footerMenu = sidebarElement.querySelector('.footer-menu___sjBQ2');
+                    if (footerMenu) {
+                        sidebarElement.insertBefore(scriptsSection, footerMenu);
+                    } else {
+                        sidebarElement.appendChild(scriptsSection);
+                    }
                 }
                 const scriptsLinkContainer = scriptsSection.querySelector('#tl-scripts-links-container');
                 if (scriptsLinkContainer) {
